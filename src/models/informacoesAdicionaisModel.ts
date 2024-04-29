a4d761648e75a7ee270b73610a29f396bc5fbe4a
Anderson Santos <andersonsantoos-10@outlook.com>
2024-04-29 19:45:27 -0300

    Entidade informações adicionais.

---
+++ src/models/informacoesAdicionaisModel.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database/dbConfig';
+import Servico from './servicoModel';
+
+class informacoesAdicionais extends Model {
+    public informacoes_id!: number;
+    public servico_id!: number;
+    public descricao!: string;
+}
+
+informacoesAdicionais.init(
+    {
+        informacoes_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        servico_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Servico',
+                key: 'servico_id'
+            },
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+        },
+        descricao: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: 'Campo não pode ser vazio'}
+            }
+        },
+    }, {
+        sequelize,
+        tableName: 'infoAdicionais'
+});
+
+informacoesAdicionais.belongsTo(Servico, {foreignKey: 'servico_id', as: 'servico'});
+
+export default informacoesAdicionais;
