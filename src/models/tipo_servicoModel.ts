c41cc8a548e2b2fb7d1f9a90822f2e0dee6bd2a8
Anderson Santos <andersonsantoos-10@outlook.com>
2024-04-29 18:25:44 -0300

    Entidade Tipo de serviço..

---
+++ src/models/tipo_servicoModel.ts
@@ -0,0 +1,42 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database/dbConfig';
+import Servico from './servicoModel';
+
+class Tipo_servico extends Model {
+    public tipo_servico_id!: number;
+    public servico_id!: number;
+    public tipo!: string;
+}
+
+Tipo_servico.init(
+    {
+        tipo_servico_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        servico_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Servico',
+                key: 'servico_id'
+            },
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+        },
+        tipo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: { msg: 'Campo não pode ser vazio '}
+            }
+        },
+    }, {
+        sequelize,
+        tableName: 'tipo_servico'
+});
+
+Tipo_servico.belongsTo(Servico, { foreignKey: 'servico_id', as: 'servico'});
+
+export default Tipo_servico;
