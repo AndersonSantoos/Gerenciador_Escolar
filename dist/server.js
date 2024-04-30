2a9b7fb0903ab55a7785eedeeba4d0931a04b66c
Anderson Santos <andersonsantoos-10@outlook.com>
2024-04-30 17:54:26 -0300

    Update

--- dist/server.js
+++ dist/server.js
@@ -11,6 +11,7 @@ const cargoRoutes_1 = __importDefault(require("./routes/cargoRoutes"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(body_parser_1.default.json());
+//Routes
 app.use('/', funcionarioRoutes_1.default);
 app.use('/', cargoRoutes_1.default);
 app.listen(port, async () => {
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,9 @@ import cargooRoutes from './routes/cargoRoutes';
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(bodyParser.json());
+//Routes
 app.use('/', funcionarioRoutes);
 app.use('/', cargooRoutes);
 
