de508547414bf5e86edada92e7cf95390e86604b
Anderson Santos <andersonsantoos-10@outlook.com>
2024-05-13 16:43:50 -0300

    Migration: filial_service, filial.

---
+++ dist/migrations/20240513194152-filial_servico_table.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('filialServico', {
+            id: {
+                type: sequelize_1.DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            nome: {
+                type: sequelize_1.DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: { msg: 'Campo não pode estar vazio' }
+                }
+            },
+            createdAt: {
+                type: sequelize_1.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updatedAt: {
+                type: sequelize_1.DataTypes.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('filialServico');
+    }
+};
---
+++ src/migrations/20240513194010-filial_table.ts
@@ -0,0 +1,34 @@
+import { QueryInterface, DataTypes } from 'sequelize';
+
+module.exports = {
+  up: async (queryInterface: QueryInterface, Sequelize: any) => {
+    await queryInterface.createTable('Filial', {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: 'Campo não pode estar vazio' }
+        }
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface: QueryInterface, Sequelize: any) => {
+    await queryInterface.dropTable('Filial');
+  }
+};
